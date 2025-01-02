755ebbcfc023d7278d6dbe466e6e1137f58f8d4e
anandashin <shinjigo221@snu.ac.kr>
2025-01-02 22:48:17 +0900

    fix: token_expires time 추가

--- watchapedia/auth/settings.py
+++ watchapedia/auth/settings.py
@@ -5,8 +5,8 @@ from datetime import timedelta
 class JWTSettings(BaseSettings):
     secret_key: str = ""
     algorithm: str = ""
-    access_token_expires_minutes: int = 0
-    refresh_token_expires_hours: int = 0
+    access_token_expires_minutes: int = 10
+    refresh_token_expires_hours: int = 24
 
     # @property
     # def secret_key(self) -> str:
