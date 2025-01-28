a8206e4d84d509223482337f11a0af954b42d854
deveroskp <imsmile2004@naver.com>
2025-01-29 01:20:32 +0900

    Fix: 오류 수정

--- watchapedia/app/auth/views.py
+++ watchapedia/app/auth/views.py
@@ -16,6 +16,28 @@ KAKAO_AUTH_URL = "https://kauth.kakao.com/oauth/authorize"
 #     # scope : openid, profile, email
 #     return RedirectResponse(url=f"{GOOGLE_AUTH_URL}?client_id={OAUTH_SETTINGS.google_client_id}&redirect_uri={OAUTH_SETTINGS.google_client_id}&response_type=code&scope=openid%20profile%20email")
 
+# @auth_router.get("/google/callback")
+# async def signin_google(
+#     response: JSONResponse,
+#     code: str,
+#     auth_service: Annotated[AuthService, Depends()]
+#     ):
+#     user_info = await auth_service.get_google_user_info(code)
+#     username = user_info["email"].split("@")[0]
+#     profile_url = user_info.get("picture")
+#     access_token, refresh_token, user_id = auth_service.signin(username=username, login_id=user_info["email"], profile_url=profile_url)
+
+#     response.set_cookie(
+#         key="refresh_token",
+#         value=refresh_token,
+#         httponly=True,
+#         expires=JWT_SETTINGS.refresh_token_expires_hours*3600,
+#         samesite="lax",
+#     )
+    
+#     return UserSigninResponse(access_token=access_token, refresh_token=refresh_token, user_id=user_id)
+
+
 @auth_router.get("/google")
 async def signin_google(
     response: JSONResponse,
